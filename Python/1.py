5538df68d688426f12aab2d48623e9d13092d000
sanagala ganesh <sanagalaganesh26@gmail.com>
2025-09-14 18:06:03 +0530

    .

--- Python/1.py
+++ Python/1.py
@@ -1,13 +1,2 @@
-l=sorted([2,2,3,3,1])
-c=0
-for i in range(len(l)):
-    print("___________")
-    print("i:", l[i])
-    print("___________")
-    for j in range(i+1,len(l)):
-        print("j:",l[j])
-        if l[i]==l[j]:
-            c=c+1
-            d={i:j}
-
-
+import keyword as l
+print(l.kwlist)
