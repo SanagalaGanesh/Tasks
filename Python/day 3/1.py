93ee43daed88b3215f445260049876ac7c51f1da
sanagala ganesh <sanagalaganesh26@gmail.com>
2025-09-29 11:10:03 +0530

    .

---
+++ Python/day 3/1.py
@@ -0,0 +1,15 @@
+# num1=int(input("Enter Num 1:"))
+# num2=int(input("Enter Num 2:"))
+# print("add :",num1+num2)
+# print("sub :",num1-num2)
+# print("mul :",num1*num2)
+# print("div :",num1/num2)
+# print("mod :",num1%num2)
+# print("floor :",num1//num2)
+
+username=input("Enter Username: ")
+password=input("Enter Password: ")
+if username=="Ganesh" and password=="Hello!World@":
+    print("Login Successfull !")
+else:
+    print("Try Again !")
