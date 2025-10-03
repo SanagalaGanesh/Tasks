9b4687fb44496b86c04a637dbb1cd6fb85b3ae13
sanagala ganesh <sanagalaganesh26@gmail.com>
2025-10-03 13:31:29 +0530

    .

---
+++ Python/Tasks/solution.py
@@ -0,0 +1,69 @@
+# 1
+
+# a=int(input("Enter Side Of a Square :"))
+# print("Area Of A Square Is",a*a)
+
+# 2
+
+# a=int(input("Enter Length : "))
+# b=int(input("Enter Breadth : "))
+# print("Area Of A Rectangle Is",a*b)
+
+# 3
+
+# a=int(input("Enter Base : "))
+# b=int(input("Enter Height : "))
+# print("Area Of Triangle Is", (1/2)*a*b)
+
+# 4
+
+# a=int(input("Enter Side : "))
+# print("Perimeter Of Square Is",4*a)
+
+# 5
+
+# a=int(input("Enter Length : "))
+# b=int(input("Enter Breadth : "))
+# print("Perimeter Of A Rectangle Is",2*(a+b))
+
+# 6
+
+# a=int(input("Enter Side 1 : "))
+# b=int(input("Enter Side 2 : "))
+# c=int(input("Enter Side 3 : "))
+# print("Perimeter Of A Triangle Is",a+b+c)
+
+# 7 
+
+# a=int(input("Enter Amount : "))
+# b=(a//1000)*1000
+# print("1000's:",a//1000)
+# print("500 's:",(a-b)//500)
+# print("Remaining :",a%500)
+
+# 8
+
+# a=int(input("Enter Seconds :"))
+# b=a//3600
+# print("Hours   :",b)
+# if b>=1:
+#     a=a-(3600*b)
+# d=a//60
+# if d>=1:
+#     a=a-(60*d)
+# print("Minutes :",d)
+# print("Seconds :",a)
+
+# 9
+
+# a=int(input(("Enter Maths Marks :")))
+# b=int(input(("Enter Physics Marks :")))
+# c=int(input(("Enter Chemistry Marks :")))
+# print("Sum Of Marks : ",a+b+c)
+
+#10
+
+# a=int(input(("Enter Maths Marks :")))
+# b=int(input(("Enter Physics Marks :")))
+# c=int(input(("Enter Chemistry Marks :")))
+# print("Average Of Marks : ",(a+b+c)/3)
